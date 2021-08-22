3a6ddc99e806316417cdad874b67a8914d2ed5c6
luke_bassett <bassett.luke@gmail.com>
2021-08-22 12:38:39 -0700

    added logging

--- color_scanner.py
+++ color_scanner.py
@@ -1,11 +1,29 @@
 import csv
 from datetime import datetime
+import logging
 import os
 import threading
 import time
 
 from PIL import Image
 import requests
+
+
+logging.basicConfig(
+    filename='color_scanner.log',
+    level=logging.INFO,
+    format='%(asctime)s | %(levelname)s | %(threadName)s | %(message)s'
+)
+
+
+def log_info(func):
+    """decorator function for logging"""
+    def wrapper(*args, **kwargs):
+        logging.info(f'Starting {func.__name__}')
+        val = func(*args, **kwargs)
+        logging.info(f'Finished {func.__name__}')
+        return val
+    return wrapper
 
 
 def load_urls(data_path='sample_data//input.txt'):
@@ -26,27 +44,29 @@ def rgb_to_hex(r, g, b):
     return r"#{:02X}{:02X}{:02X}".format(r, g, b)
 
 
+@log_info
+def load_image(url):
+    return Image.open(requests.get(url, stream=True).raw)
+
+
 class ColorScanner():
-    def __init__(self, urls, fname, limit=None, n_colors=3, write_freq=25):
+    def __init__(self, urls, fname, n_colors=3, write_freq=25):
         self.urls = urls
         self.n_colors = n_colors
         self.results = []
         self.fname = fname
         self.write_freq = write_freq
-        if limit:
-            self.urls = self.urls[:limit]
 
     def scan(self):
-        color_count_thread = None
-        for url in self.urls:
+        for i, url in enumerate(self.urls):
+            img = load_image(url)
 
-            img = Image.open(requests.get(url, stream=True).raw)
-
-            if color_count_thread is not None:
+            if i == 0:
+                color_count_thread = None
+            else:
                 color_count_thread.join()
-
-            if len(self.results) >= self.write_freq:
-                self.write_results()
+                if len(self.results) >= self.write_freq:
+                    self.write_results()
 
             color_count_thread = threading.Thread(
                 target=self.get_top_colors,
@@ -58,6 +78,7 @@ class ColorScanner():
         if len(self.results) > 0:
             self.write_results()
 
+    @log_info
     def get_top_colors(self, url, img):
         top_colors = find_top_colors(img)
         if top_colors:  # image not removed
@@ -65,7 +86,9 @@ class ColorScanner():
         else:
             self.results.append([url])
 
+    @log_info
     def write_results(self):
+        logging.info(f"Writing {len(self.results)} results to {self.fname}")
         with open(self.fname, 'a') as csvfile:
             writer = csv.writer(csvfile, dialect='unix')
             writer.writerows(self.results)
@@ -74,11 +97,8 @@ class ColorScanner():
 
 if __name__ == '__main__':
     t1 = time.perf_counter()
-    t = datetime.now()
-    fname = t.strftime("results_%Y%m%d_%H%M%S.csv")
-
-    cs = ColorScanner(urls=load_urls(), fname=fname, limit=5)
+    fname = datetime.now().strftime("results_%Y%m%d_%H%M%S.csv")
+    cs = ColorScanner(urls=load_urls()[:50], fname=fname)
     cs.scan()
-
     t2 = time.perf_counter()
     print(f'{t2 - t1} seconds')
